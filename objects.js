d2c3308e6194bad24e8427b0ba28dcb6fa427a99
Samiur Rashid <samrashid@gmail.com>
2017-10-16 16:11:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {
+<<<<<<< HEAD
   Killers: "Where You Were Young"
 };
 
@@ -10,4 +11,11 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist
+=======
+  artist: "Attention"
+};
+
+updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName, songTitle]
+>>>>>>> 146760b4082e80a71bce052df1d2d1c2db02b978
 }
